54108c54fd369497d76c7df01969dee0124444ec
HodaRoushdy <hodaroshdy106@gmail.com>
2023-11-13 18:13:26 +0200

    final

---
+++ app/components/cell.tsx
@@ -0,0 +1,43 @@
+
+import { Dispatch, SetStateAction } from "react";
+
+type cellProps={
+    go:string;
+    setGo:Dispatch<SetStateAction<string>>;
+    id:number;
+    cells:string[];
+    setCells:Dispatch<SetStateAction<string[]>>;
+    cell:string;
+    winMsg:string
+}
+const cell =({go,setGo,id,cells,setCells,cell,winMsg}:cellProps)=>{
+
+    const handleClick =(e)=>{
+        if(winMsg){
+            return
+        }
+        const taken = !!cells[id]
+        if(!taken){
+            if(go === "circle"){
+                changeCellVal("circle")
+                setGo("cross")
+            }else if (go === "cross"){
+                changeCellVal("cross")
+                setGo("circle")
+            }
+        }
+        }
+    const changeCellVal =(val:string)=>{
+        const copyCells =[...cells]
+        copyCells[id]=val
+        setCells(copyCells)
+    }
+
+return(
+    <div className="square" onClick={handleClick}>
+        <div className={cell}> {cell ? (cell === 'circle' ? 'O' : 'X') : ''}</div>
+    </div>
+)
+}
+export default cell
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Cell from './components/cell'
 
 export default function Home() {
+const [cells,setCells] = useState(['','','','','','','','',''])
+const [go,setGo]=useState('cross')
+const winningProbab =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+const [winMsg,setwinMsg] = useState('')
+useEffect(()=>{
+  winningProbab.forEach((probab)=>{
+  const circleWin = probab.every((cell)=> cells[cell] === 'circle' )
+  const crossWin = probab.every((cell)=> cells[cell] === 'cross')
+  if(circleWin){
+    setwinMsg('the winner is circle')
+  }else if(crossWin){
+    setwinMsg('the winner is cross')
+  }
+})
+
+},[cells])
+
+useEffect(()=>{
+  if(cells.every((cell)=> cell !="" ) && !winMsg) 
+  {setwinMsg('draw!!')} 
+
+
+},[cells,winMsg])
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className='fullScreen'>
+    <div className="gameBoard">
+      {cells.map((cell,index)=>(
+      <Cell
+      cell={cell}
+      id={index} 
+      go={go} 
+      setGo={setGo} 
+      key={index}
+      cells={cells}
+      setCells={setCells}
+      winMsg={winMsg}/>
+))}
+</div>
+<div>
+  <h1 className="sentance">{`${winMsg}`}</h1>
+  {!winMsg && <h1 className="sentance">{`it's now ${go} turn`}</h1> }
+  </div>
     </main>
   )
 }
